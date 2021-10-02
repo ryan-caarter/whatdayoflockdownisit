ea93b3465223f5dfea944914db2ab67d483c5afb
ryancarter <ryan.c4rter@gmail.com>
2021-10-03 02:24:03 +1300

    daily update

--- update.py
+++ update.py
@@ -27,3 +27,19 @@ for filename in os.listdir(folder_path):
             file.close()
 
 print("Updated site and cleaned!")
+from git import Repo
+
+PATH_OF_GIT_REPO = '.git'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'daily update'
+
+def git_push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        origin = repo.remote(name='origin')
+        origin.push()
+    except:
+        print('Some error occured while pushing the code')
+
+git_push()
