7675a03894720677f2582b0aa520a0c8dc4f7b77
ryancarter <ryan.c4rter@gmail.com>
2021-10-03 02:33:01 +1300

    daily update

--- automate.sh
+++ automate.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd /Users/ryancarter/Documents/Programs/WDOLDII/whatdayoflockdownisit/
 /Library/Frameworks/Python.framework/Versions/3.7/bin/python3 update.py
+git add *
+git commit -m "daily update"
+git push
--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 import bs4
 import os
 import sys
-from git import Repo
 import requests
 
 folder_path = "." if len(sys.argv) == 1 else sys.argv[1]
@@ -27,19 +26,3 @@ for filename in os.listdir(folder_path):
             file.close()
 
 print("Updated site and cleaned!")
-from git import Repo
-
-PATH_OF_GIT_REPO = '.git'  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'daily update'
-
-def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='origin')
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')
-
-git_push()
