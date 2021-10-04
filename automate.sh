6e74b23bde8a10d0e4de2a25d7e8b96623f4fa53
ryancarter <ryan.c4rter@gmail.com>
2021-10-04 21:49:32 +1300

    obfuscating

--- automate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HOME=/Users/ryancarter
-cd /Users/ryancarter/Documents/Programs/WDOLDII/whatdayoflockdownisit/
-/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 update.py $1
-git add *
-git commit -m "daily update"
-git push
-echo "It's been pushed 😁"
